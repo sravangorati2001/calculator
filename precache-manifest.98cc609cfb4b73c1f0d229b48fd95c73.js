539feeb6a32d62cd4a6ed4bc49cd3314bd941940
Sravan Kumar <sravangorati2001@gmail.com>
2020-09-27 19:28:29 +0530

    Updates

---
+++ precache-manifest.98cc609cfb4b73c1f0d229b48fd95c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b34ee4a8984433533d4e74a3e954fb",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1f35ab8b1890c8fe669d",
+    "url": "/calculator/static/css/main.2e4a643f.chunk.css"
+  },
+  {
+    "revision": "6ffb04eaa33be27101f7",
+    "url": "/calculator/static/js/2.366be3f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.366be3f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f35ab8b1890c8fe669d",
+    "url": "/calculator/static/js/main.4b2200c5.chunk.js"
+  },
+  {
+    "revision": "acc22484ce2a70562870",
+    "url": "/calculator/static/js/runtime-main.09cf1221.js"
+  }
+]);
